5e91d9a415449b2b6aad9965a55a2bed33e54e27
Kirsten Naughton <kirsten.naughton@gmail.com>
2017-11-04 10:58:52 -0700

    adding files used at state tournament used at ASU in Oct 2017

---
+++ constants-ASUtournament.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team6372.robot;
+
+import edu.wpi.first.wpilibj.Talon;
+
+public class Constants {
+
+  public static final int TALON_L2_PORT = 2;
+  public static final int TALON_R2_PORT = 4;
+  public static final int TALON_LC_PORT = 3;
+  public static final int TALON_RC_PORT = 1;
+  public static final int JOYSTICK_PORT = 0;
+  public static final int XBOX_CONTROLLER_PORT = 1;
+  public static final double MAX_DRIVE_SPEED = .8;
+  public static final double MAX_TURN_SPEED = .4;
+  public static final double MAX_CLIMB_SPEED = .5;
+  public static final int JOYSTICK_LEFT_DRIVE_AXIS = 1;
+  public static final int JOYSTICK_RIGHT_DRIVE_AXIS = 1;
+  public static final int JOYSTICK_TURN_DRIVE_AXIS = 2;
+  public static final int CONTROLLER_CLIMB_AXIS = 3;
+  public static final int CAMERA_SERVER = 0;
+  public static final int RESOLUTION = 360;
+  public static final int RESOLUTION_1 = 480;
+}
